8d346bde0940eee8f3a4f8a89008353f604a8d98
steverx <64512884+steverx@users.noreply.github.com>
2021-12-16 14:47:42 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
-
 ReactDOM.render(
     <App />,
   document.getElementById('root')
